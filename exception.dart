fda9f7afee4623c8ba13e06d7c91ac27b5e53fe4
Marciano Pereira <43121709+PereiraSantos@users.noreply.github.com>
2021-10-03 18:23:18 -0300

    Update exception.dart

--- exception.dart
+++ exception.dart
@@ -2,3 +2,21 @@
 class MyException implements Exception{
   String err() => "erro interno";
 }
+
+import 'package:padrao_projeto/excepition/exception.dart';
+
+void main() {
+  try{
+    testeException("erro");
+  }
+  on MyException catch(e){
+    print(e.err());
+  }
+
+}
+
+void testeException(String teste){
+  if(teste == "erro"){
+    throw new MyException();
+  }
+}
