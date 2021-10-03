41a1721bfdcdc36cfeb6be97da4288895de18436
Marciano Pereira <43121709+PereiraSantos@users.noreply.github.com>
2021-10-03 18:23:57 -0300

    Update singleton.dart

--- singleton.dart
+++ singleton.dart
@@ -1,15 +1,9 @@
 class SingletonPessoa {
 
-  static SingletonPessoa? _instance;
+ static SingletonPessoa? _instance;
+  
 @override
   SingletonPessoa._internal();
-
-/*
-  static get instance{
-    _instance ?? SingletonPessoa._internal();
-    return _instance;
-  }
-*/
 
   factory SingletonPessoa(){
     _instance ?? SingletonPessoa._internal();
